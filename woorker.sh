9d88f0a342805a45bb2545332e1d8c00d1c7934a
somees2 <latirer8@gmail.com>
2024-05-30 22:55:29 +0530

    Update woorker.sh

--- woorker.sh
+++ woorker.sh
@@ -1,1 +1,1 @@
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user LTC:ML2GoqRmRtzdKwLf1GHknQdSbvusfCfrLs.q20o-fite --ethstratum ETHPROXY
+./1.88/./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user LTC:ML2GoqRmRtzdKwLf1GHknQdSbvusfCfrLs.q20o-fite --ethstratum ETHPROXY
